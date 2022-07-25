749b5afe94698a484bd24ccb76b5aa303e222ceb
zaen19-R <sangpnikmat@gmail.com>
2022-07-26 00:21:47 +0700

    add directory

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+    arrowParens: 'avoid',
+    singleQuote: true,
+    backetSpacing: true,
+    endOfline: 'lf',
+    semi: false,
+    tabWidth: 2,
+    trailingComma: 'none'
+}
+
+module.exports= options
